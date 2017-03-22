2557af710925522a669edce6f42a9a49fc33486f
darolb <159houng@naver.com>
2017-03-22 16:45:51 +0900

    project updt

---
+++ pinch example/Assets/make_script/makecube.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class makecube : MonoBehaviour {
+	private float speedX;
+	private float speedY;
+	public Material mat;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Click(){
+		speedX = Random.Range (-1f, 1f);
+		speedY = Random.Range (-1f, 1f);	
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.position = new Vector3(speedX,speedY,1);
+		cube.transform.localScale = new Vector3 (0.1f, 0.1f, 0.1f);
+	
+	}
+}
---
+++ pinch example/Assets/resourse/test001.shader
@@ -0,0 +1,14 @@
+﻿Shader "Custom/test001" {
+    Properties {
+        _MainTex ("Texture to blend", 2D) = "black" { }
+    }
+
+    SubShader {
+    	Tags { "Queue" = "Transparent"}
+        Pass {
+        	Blend One One
+        	SetTexture [_MainTex] { combine texture }
+        }
+    }
+ 
+}
